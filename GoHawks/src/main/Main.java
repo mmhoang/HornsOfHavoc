5cf3047de241d56c7a249658bbf6829dc71f9008
mhoangcs <mhoangcss@gmail.com>
2016-01-08 00:14:54 -0800

    Added some comments.

--- GoHawks/src/main/Main.java
+++ GoHawks/src/main/Main.java
@@ -1,10 +1,12 @@
 package main;
 
+// Prints Seahawks slogans.
 public class Main {
 
+	// The main entry point for the program.
 	public static void main(String[] args) {
 		System.out.println("Go Hawks!");
-
+		
 	}
 
 }
