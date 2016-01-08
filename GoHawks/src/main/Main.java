c41199cd5a31bcb27cf1f63ee4d1bd723eda551b
mhoangcs <mhoangcss@gmail.com>
2016-01-08 00:26:07 -0800

    Added a hashtag, changed a comment.

--- GoHawks/src/main/Main.java
+++ GoHawks/src/main/Main.java
@@ -1,12 +1,12 @@
 package main;
 
-// Prints Seahawks slogans.
+// Prints Seahawks hashtags.
 public class Main {
 
 	// The main entry point for the program.
 	public static void main(String[] args) {
 		System.out.println("Go Hawks!");
-		
+		System.out.println("Blue Friday");
 	}
 
 }
