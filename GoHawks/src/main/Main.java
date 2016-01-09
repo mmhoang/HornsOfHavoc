b0531a70b493a1f5058ab1792033172734f01fc7
mhoangcs <mhoangcss@gmail.com>
2016-01-08 16:02:51 -0800

    Removed BlueFriday, added another comment.

--- GoHawks/src/main/Main.java
+++ GoHawks/src/main/Main.java
@@ -6,7 +6,9 @@ public class Main {
 	// The main entry point for the program.
 	public static void main(String[] args) {
 		System.out.println("Go Hawks!");
-		System.out.println("Blue Friday");
+		
+		// Comment
+		
 	}
 
 }
