9636a6f61dd8aa69887847e36aa17f0c9fcd90d8
Margaret Hoang <mhoangcss@gmail.com>
2016-01-18 18:27:31 -0800

    Small adjustments to the sample code.  Right wheels work!

--- Getting Started/src/org/usfirst/frc/team3393/robot/Robot.java
+++ Getting Started/src/org/usfirst/frc/team3393/robot/Robot.java
@@ -22,7 +22,7 @@ public class Robot extends IterativeRobot {
      * used for any initialization code.
      */
     public void robotInit() {
-    	myRobot = new RobotDrive(0,1);
+    	myRobot = new RobotDrive(1,0);
     	stick = new Joystick(0);
     }
     
@@ -39,7 +39,7 @@ public class Robot extends IterativeRobot {
     public void autonomousPeriodic() {
     	if(autoLoopCounter < 100) //Check if we've completed 100 loops (approximately 2 seconds)
 		{
-			myRobot.drive(-0.5, 0.0); 	// drive forwards half speed
+			myRobot.drive(-0.5, -5.0); 	// drive forwards half speed
 			autoLoopCounter++;
 			} else {
 			myRobot.drive(0.0, 0.0); 	// stop robot
