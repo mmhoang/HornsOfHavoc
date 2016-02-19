9c04cf62c5b69a1aeaa37f29faeb9d31c25418ed
Arel Foster <arelfoster@ymail.com>
2016-02-18 20:02:19 -0800

    worked on auto 2

--- Getting Started/src/org/usfirst/frc/team3393/robot/Robot.java
+++ Getting Started/src/org/usfirst/frc/team3393/robot/Robot.java
@@ -15,49 +15,50 @@ import edu.wpi.first.wpilibj.livewindow.LiveWindow;
  * directory.
  */
 public class Robot extends IterativeRobot {
-	RobotDrive myRobot; 
+	RobotDrive myRobot;
 	Joystick left, right, control;
 	Solenoid downLift, upLift;
-	Solenoid frontDownLift, frontUpLift; 
+	Solenoid frontDownLift, frontUpLift;
 	int toggle;
 	Victor shooter;
 	Victor climber1;
 	Victor climber2;
 	Timer _aTimer;
 	Accelerometer _accelerometer;
-	
+
 	AnalogGyro _gyro;
-	
+
 	double _velocity;
-	double _displacement; 
+	double _displacement;
 	Timer _eTimer;
- 
+
 	/**
 	 * This function is run when the robot is first started up and should be
 	 * used for any initialization code.
-	 */ 
+	 */
 	public void robotInit() {
-		toggle = 1;
+		toggle = 2;
 		shooter = new Victor(6);
 		climber1 = new Victor(4);
 		climber2 = new Victor(5);
-		frontDownLift = new Solenoid(2); //4 and 5
+		frontDownLift = new Solenoid(2); // 4 and 5
 		frontUpLift = new Solenoid(3);
-		myRobot = new RobotDrive(0, 1, 2, 3); // Added Motors 2 and 3 to make all motors run
+		myRobot = new RobotDrive(0, 1, 2, 3); // Added Motors 2 and 3 to make
+												// all motors run
 		right = new Joystick(1);
 		left = new Joystick(3);
-		downLift = new Solenoid(0); //6 and 7
+		downLift = new Solenoid(0); // 6 and 7
 		upLift = new Solenoid(1);
-		
+
 		this._aTimer = new Timer();
 		this._accelerometer = new BuiltInAccelerometer();
-		
+
 		this._gyro = new AnalogGyro(0);
-		
+
 		this._velocity = 0;
 		this._displacement = 0;
 		this._eTimer = new Timer();
-		
+
 	}
 
 	/**
@@ -65,11 +66,11 @@ public class Robot extends IterativeRobot {
 	 */
 	public void autonomousInit() {
 		this._aTimer.start();
-		
+
 		this._gyro.reset();
-		
+
 		this.resetDistance();
-		
+
 		this._eTimer.start();
 	}
 
@@ -78,7 +79,7 @@ public class Robot extends IterativeRobot {
 	 */
 	public void autonomousPeriodic() {
 		this.updateDistance();
-		
+
 		if (toggle == 1) {
 			autonomous1();
 		} else if (toggle == 2) {
@@ -97,16 +98,16 @@ public class Robot extends IterativeRobot {
 	 * 
 	 * \
 	 */
-	public void teleopInit() { 
-			 
+	public void teleopInit() {
+
 	}
-	
+
 	/**
 	 * This function is called periodically during operator control
 	 */
 	public void teleopPeriodic() {
 		this.updateDistance();
-		
+
 		myRobot.tankDrive(left, right);
 
 		// This works!
@@ -115,38 +116,37 @@ public class Robot extends IterativeRobot {
 		// (a WHILE loop would be stuck until the button was released)
 		if (right.getRawButton(3)) {
 			shooter.set(1.0);
-		}
-		else if (right.getRawButton(2)) {
+		} else if (right.getRawButton(2)) {
 			shooter.set(-1.0);
-		}
-		else shooter.set(0.0);
+		} else
+			shooter.set(0.0);
 
-		if (left.getRawButton(4)) { //fork pneumatics 
+		if (left.getRawButton(4)) { // fork pneumatics
 			downLift.set(true);
 			upLift.set(false);
-		} else if (left.getRawButton(5)) { 
+		} else if (left.getRawButton(5)) {
 			downLift.set(false);
 			upLift.set(true);
 		}
-		if (right.getRawButton(4)) { //ball lifting 
+		if (right.getRawButton(4)) { // ball lifting
 			frontDownLift.set(true);
 			frontUpLift.set(false);
 		} else if (right.getRawButton(5)) {
 			frontDownLift.set(false);
 			frontUpLift.set(true);
 		}
-		if (right.getRawButton(7)){
+		if (right.getRawButton(7)) {
 			climber1.set(1.0);
 			climber2.set(1.0);
-	
-		} else if (right.getRawButton(8)){
+
+		} else if (right.getRawButton(8)) {
 			climber1.set(-1.0);
-			climber2.set(-1.0);	
+			climber2.set(-1.0);
 		} else {
 			climber1.set(0);
 			climber2.set(0);
 		}
-		
+
 		// //if(left.getRawbutton(x)){
 		// Relay.set(Relay.Value.bForward);
 		// }
@@ -172,48 +172,45 @@ public class Robot extends IterativeRobot {
 	}
 
 	enum Auto1State {
-		DRIVE_FORWARD,
-		CASTLE_TURN,
-		DRIVE_TO_CASTLE,
-		RELEASE_BALL,
-		FINISH
+		DRIVE_FORWARD, CASTLE_TURN, DRIVE_TO_CASTLE, RELEASE_BALL, FINISH
 	}
+
 	Auto1State auto1State = Auto1State.DRIVE_FORWARD;
+
 	private void autonomous1() {
-		if(auto1State == Auto1State.DRIVE_FORWARD) { 
+		if (auto1State == Auto1State.DRIVE_FORWARD) {
 			myRobot.tankDrive(0.6, 0.6);
-			downLift.set(true);//Splatalorx
+			downLift.set(true);// Splatalorx
 			upLift.set(false);
-			frontDownLift.set(false);//front
+			frontDownLift.set(false);// front
 			frontUpLift.set(true);
 			System.out.println("Distance" + this.getDistance());
-			
+
 			// Stop after 8 feet in meters
-			if(this.getDistance() >= 3.7) { //2.4384, 2,, 3.6576
+			if (this.getDistance() >= 3.7) { // 2.4384, 2,, 3.6576
 				// Reset distance because we're turning in the next state
-				//this.resetDistance();
+				// this.resetDistance();
 				this._aTimer.reset();
 				auto1State = Auto1State.CASTLE_TURN;
 			}
-		}else if(auto1State == Auto1State.CASTLE_TURN) {
-			myRobot.tankDrive(0.1,0.85);		//0.1,0.4
-			System.out.println("castle"+this._aTimer.get());
-			if(this._aTimer.get() >=1.5){
+		} else if (auto1State == Auto1State.CASTLE_TURN) {
+			myRobot.tankDrive(0.1, 0.85); // 0.1,0.4
+			System.out.println("castle" + this._aTimer.get());
+			if (this._aTimer.get() >= 1.5) {
 				auto1State = Auto1State.DRIVE_TO_CASTLE;
 				this.resetDistance();
 			}
-		
-		}else if (auto1State == Auto1State.DRIVE_TO_CASTLE) {
+
+		} else if (auto1State == Auto1State.DRIVE_TO_CASTLE) {
 			myRobot.tankDrive(-0.6, -0.6);
-			System.out.println("drivetocastle"+this.getDistance());
-			if(this.getDistance() >= 1.0){
+			System.out.println("drivetocastle" + this.getDistance());
+			if (this.getDistance() >= 1.0) {
 				this.resetDistance();
 				this._aTimer.reset();
 				auto1State = Auto1State.RELEASE_BALL;
-				
-				
+
 			}
-		} else if(auto1State == Auto1State.RELEASE_BALL) {
+		} else if (auto1State == Auto1State.RELEASE_BALL) {
 			myRobot.tankDrive(0.0, 0.0);
 			shooter.set(1.0);
 			if (this._aTimer.get() >= 2.0) {
@@ -223,33 +220,62 @@ public class Robot extends IterativeRobot {
 		} else if (auto1State == Auto1State.FINISH) {
 			System.out.print("IT'S OVER!");
 		}
-		
 
 	}
 
-	
+	enum Auto2State {
+		DRIVE_REVERSE, CASTLE_TURN, RELEASE_BALL, CASTLE_TURNBACK, RECROSS_BARRIER, FINISH
+	}
+
+	Auto2State auto2State = Auto2State.DRIVE_REVERSE;
+
 	private void autonomous2() {
-		if (this._aTimer.get() <= 4.0) { // going straight and crossing low bar for
-									// 16 feet
-			myRobot.drive(0.5, 0.0);
-		} else if (this._aTimer.get() <= 6.0) { // fix the odd right turn/angle.
-											// Increased autoLoopCounter
-			myRobot.drive(0.5, -0.32); // turn to fix odd right turn
-		} else if (this._aTimer.get() <= 8.0) { // Turning 135 degrees after
-											// crossing low bar to shoot
-			myRobot.drive(0.475, 1.2); // made the turn from 0.935 to whatever
-										// else
-		} else if (this._aTimer.get() <= 10.0) {// Robot is going to stop for 40
-											// loops to stop and drop ball
-			myRobot.drive(0.0, 0.0); //
-		} else if (this._aTimer.get() <= 12.0) {
-			myRobot.drive(0.475, 0.4); // made the turn from .49 to whatever
-										// else
-		} else if (this._aTimer.get() <= 14.0) { // should return to normal starting
-											// position
-			myRobot.drive(0.5, 0.0);
-		} else {
-			myRobot.drive(0.0, 0.0); // increased autoLoopCounter by 60
+		if (auto2State == Auto2State.DRIVE_REVERSE) {
+			myRobot.tankDrive(0.7, 0.7);
+			downLift.set(true);// Splatalorx
+			upLift.set(false);
+			frontDownLift.set(false);// front
+			frontUpLift.set(true);
+			System.out.println("Distance" + this.getDistance());
+			if (this.getDistance() >= 3.7) { // 2.4384, 2,, 3.6576
+				this._aTimer.reset();
+				auto2State = Auto2State.CASTLE_TURN;
+			}
+		} else if (auto2State == Auto2State.CASTLE_TURN) {
+			myRobot.tankDrive(0.6, -0.6); 
+			System.out.println("castle" + this._aTimer.get());
+			if (this._aTimer.get() >= 1.5) {
+				auto2State = Auto2State.RELEASE_BALL;
+				this.resetDistance();
+				this._aTimer.reset();
+			}
+		} else if (auto2State == Auto2State.RELEASE_BALL) {
+			myRobot.tankDrive(0.0, 0.0);
+			shooter.set(1.0);
+			if (this._aTimer.get() >= 1.5) {
+				shooter.set(0.0);
+				this._aTimer.reset();
+				auto2State = Auto2State.CASTLE_TURNBACK;
+			}
+		} else if (auto2State == Auto2State.CASTLE_TURNBACK) {
+			myRobot.tankDrive(0.62, -0.62); 
+			System.out.println("castle turn back" + this._aTimer.get());
+			if (this._aTimer.get() >= 0.71) {
+				auto2State = Auto2State.RECROSS_BARRIER;
+				this.resetDistance();
+				this._aTimer.reset();
+			}
+		} else if (auto2State == Auto2State.RECROSS_BARRIER) {
+			myRobot.tankDrive(0.7, 0.7);
+			System.out.println("Distance" + this.getDistance());
+			if (this.getDistance() >= 2.0) { // 3.7, 2.4384, 2,, 3.6576
+				auto2State = Auto2State.FINISH;
+
+			}
+
+		} else if (auto2State == Auto2State.FINISH) {
+			myRobot.tankDrive(0.0, 0.0);
+			System.out.print("IT'S OVER!");
 		}
 	}
 
@@ -259,30 +285,33 @@ public class Robot extends IterativeRobot {
 	// turn 180
 	// drive 6 ft reach barior
 	enum Auto3State {
-		DRIVE_REVERSE,  
-		FINISH
+		DRIVE_REVERSE, FINISH
 	}
+
 	Auto3State auto3State = Auto3State.DRIVE_REVERSE;
+
 	private void autonomous3() {
-		if(auto3State == Auto3State.DRIVE_REVERSE) {
+		if (auto3State == Auto3State.DRIVE_REVERSE) {
 			myRobot.tankDrive(-0.25, -0.25);
-			if(this.getDistance() >= 2.4384) {
-				
+			if (this.getDistance() >= 2.4384) {
+
 				auto3State = Auto3State.FINISH;
 			}
-		}else if(auto3State == Auto3State.FINISH) {
-			myRobot.tankDrive(0,0);
+		} else if (auto3State == Auto3State.FINISH) {
+			myRobot.tankDrive(0, 0);
 		}
 	}
-	
+
 	private void updateDistance() {
 		this._velocity += this._accelerometer.getY() * this._eTimer.get();
 		this._displacement += this._velocity * this._eTimer.get();
 		this._eTimer.reset();
 	}
+
 	private double getDistance() {
 		return Math.abs(this._displacement) * 10;
 	}
+
 	private void resetDistance() {
 		this._velocity = 0.0;
 		this._displacement = 0.0;
