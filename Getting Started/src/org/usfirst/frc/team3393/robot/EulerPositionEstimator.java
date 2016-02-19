66cab67dd51300e76f28a34019ddb9a17fa8c079
Philip Rader <pjrader1@yahoo.com>
2016-02-18 21:50:31 -0800

    Added comments to EulerPositionEstimator
    Hopefully this will give anybody curious a better idea of what is happening, as well as how to use the estimator.

--- Getting Started/src/org/usfirst/frc/team3393/robot/EulerPositionEstimator.java
+++ Getting Started/src/org/usfirst/frc/team3393/robot/EulerPositionEstimator.java
@@ -3,6 +3,14 @@ package org.usfirst.frc.team3393.robot;
 import edu.wpi.first.wpilibj.Timer;
 import edu.wpi.first.wpilibj.interfaces.Accelerometer;
 
+/**
+ * Estimates displacement and velocity using an
+ * accelerometer and Euler's method of integral
+ * approximation.
+ * 
+ * @author pjrader1
+ *
+ */
 public class EulerPositionEstimator {
 	
 	private Accelerometer _accelSensor;
@@ -11,39 +19,81 @@ public class EulerPositionEstimator {
 	private double _displacement;
 	private Timer _eTimer;
 	
+	/**
+	 * Constructs a EulerPositionEstimator.
+	 * @param accelerometer The accelerometer objects that is used to approximate
+	 */
 	public EulerPositionEstimator(Accelerometer accelerometer) {
 		this._accelSensor = accelerometer;
 		
 		this._eTimer = new Timer();
-		this._eTimer.start();
+		this._eTimer.start(); // Start the timer right away, reset everything (including timer) when an "init" is called 
 	}
 	
+	/**
+	 * Gets the reference for the accelerometer used for approximation.
+	 * @return Accelerometer used for approximation
+	 */
 	public Accelerometer getAccelerometer() {
 		return this._accelSensor;
 	}
 	
+	/**
+	 * Updates the velocity and displacement. Should be called
+	 * once and only once during each periodic (step).
+	 */
 	public void update() {
+		// Acceleration is measured in m/s/s,
+		// multiply acceleration by seconds and you
+		// are left with m/s (velocity)
 		this._velocity += this._accelSensor.getY() * this._eTimer.get();
+		// Velocity is measured in m/s,
+		// multiply velocity by seconds and you
+		// are left with m (displacement)
 		this._displacement += this._velocity * this._eTimer.get();
+		// Reset the timer for the next step (delta time)
 		this._eTimer.reset();
 	}
 	
+	/**
+	 * Resets the displacement and velocity to zero.
+	 */
 	public void reset() {
 		this._velocity = 0;
 		this._displacement = 0;
 		this._eTimer.reset();
 	}
 	
+	/**
+	 * Gets the current approximated displacement (distance with direction)
+	 * in meters. Error increases over time.
+	 * @return Current approximated displacement in meters
+	 */
 	public double getDisplacement() {
 		return this._displacement * 10;
 	}
+	/**
+	 * Gets the current approximated distance in meters. Error
+	 * increases over time.
+	 * @return Current approximated distance in meters
+	 */
 	public double getDistance() {
 		return Math.abs(this._displacement) * 10;
 	}
 	
+	/**
+	 * Gets the current approximated velocity (speed with direction)
+	 * in meters per second. Error incraeses over time.
+	 * @return Current approximated velocity in m/s
+	 */
 	public double getVelocity() {
 		return this._velocity * 10;
 	}
+	/**
+	 * Gets the current approximated speed in meters per
+	 * second. Error increases over time.
+	 * @return Current approximated speed in m/s
+	 */
 	public double getSpeed() {
 		return Math.abs(this._velocity) * 10;
 	}
